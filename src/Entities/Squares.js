bb50540999feb53352785743f84cca8a5268f44b
Digital Resistance <digital.resistance.666@email>
2018-09-02 00:30:28 +0200

    some work on squares

--- src/Entities/Squares.js
+++ src/Entities/Squares.js
@@ -14,21 +14,30 @@ class Squares extends Entity {
 
   render(ctx, dt, ms) {
     this.beginRender(ctx);
+    var i = floor(ms/1000);
     times(SQUARES_ROWS, (row)=> {
-      times(SQUARES_COLUMNS, (col)=> this.drawSquare(ctx, row, col));
+      times(SQUARES_COLUMNS, (col)=> this.drawSquare(ctx, row, col, i++));
     });
     this.endRender(ctx);
   }
 
-  drawSquare(ctx, row, col) {
+  drawSquare(ctx, row, col, i) {
     var x = modulate(col, -SQUARES_W/2, SQUARES_W/2, 0, SQUARES_COLUMNS),
-        y = modulate(row, -SQUARES_H/2, SQUARES_H/2, 0, SQUARES_ROWS);
+        y = modulate(row, -SQUARES_H/2, SQUARES_H/2, 0, SQUARES_ROWS),
+        r = floor(sin(i))*(PI2/4);
 
     ctx.fillStyle = (this.p.c === 0) ? COLOR_BLACK : COLOR_LIGHTGRAY;
     ctx.translate(x, y);
+    ctx.rotate(r);
     ctx.fillRect(-SQUARES_SIZE/2, -SQUARES_SIZE/2, SQUARES_SIZE, SQUARES_SIZE);
     ctx.fillStyle = (this.p.c === 0) ? COLOR_LIGHTGRAY : COLOR_BLACK;
-    this[['drawCircle', 'drawHalfCircles', 'drawTriangle'][row]](ctx);
+    this[[
+      'drawCircle',
+      'drawHalfCircles',
+      'drawTriangle',
+      'drawTriangles',
+    ][i%4]](ctx);
+    ctx.rotate(-r);
     ctx.translate(-x, -y);
   }
 
@@ -54,5 +63,19 @@ class Squares extends Entity {
     ctx.lineTo(-SQUARES_R, SQUARES_R);
     ctx.closePath();
     ctx.fill();
+  }
+
+  drawTriangles(ctx) {
+    times(4, (i)=> {
+      var t = [i%2, floor(i/2)].map((v)=>-SQUARES_R/2+v*SQUARES_R);
+      ctx.translate(t[0], t[1]);
+      ctx.beginPath();
+      ctx.moveTo(SQUARES_R/2, -SQUARES_R/2);
+      ctx.lineTo(SQUARES_R/2, SQUARES_R/2);
+      ctx.lineTo(-SQUARES_R/2, SQUARES_R/2);
+      ctx.closePath();
+      ctx.fill();
+      ctx.translate(-t[0], -t[1]);
+    });
   }
 }
